4bacef404e822c4f6906fbf3d8bb8327b5b6cbb6
w1n5t0n <w1n5t0n@lnfinitemonkeys.org>
2025-03-09 21:49:27 +0000

    Change build.sh script to rm docs directory before building.

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 # Build the ClojureScript code in advanced mode
 clojure -M:release
 
-# Copy the static assets from resources/public to docs
-cp -r resources/public/* docs/
+rm -rf docs/*
+
+# Create the 'docs' directory if it does not exist and copy the static assets from resources/public to docs
+mkdir -p docs && cp -r resources/public/* docs/
 
 echo "Build complete! Files are ready in docs/ for GitHub Pages"
